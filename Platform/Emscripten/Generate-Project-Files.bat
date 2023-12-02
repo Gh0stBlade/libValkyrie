d763ae9445ff2222a2ce8a24c5eccfbadbdaf81e
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2023-12-02 17:40:23 +0000

    [Project]: Fix emscripten compile.

--- Platform/Emscripten/Generate-Project-Files.bat
+++ Platform/Emscripten/Generate-Project-Files.bat
@@ -1,2 +1,7 @@
-cd ../../../../
-emcmake cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_SHARED_LINKER_FLAGS="-sEXTRA_EXPORTED_RUNTIME_METHODS=Emulator_SetFocus"  -DCMAKE_EXE_LINKER_FLAGS="-sEXPORTED_RUNTIME_METHODS=ccall,cwrap -sEXTRA_EXPORTED_RUNTIME_METHODS=_GAMELOOP_RequestLevelChange" -B%cd%/Build/Build_EMSDK/
+set PATH=%PATH%;%~dp0../../../emscripten
+cd ../../../emsdk
+call emsdk install latest
+call emsdk activate latest
+call emsdk_env
+cd ../../
+emcmake cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_SHARED_LINKER_FLAGS="-sEXTRA_EXPORTED_RUNTIME_METHODS=Emulator_SetFocus"  -DCMAKE_EXE_LINKER_FLAGS="-sEXPORTED_RUNTIME_METHODS=ccall,cwrap -sEXTRA_EXPORTED_RUNTIME_METHODS=_GAMELOOP_RequestLevelChange" -B%cd%/Build/Build_EMSDK/ && cmake --build %cd%/Build/Build_EMSDK/ 
